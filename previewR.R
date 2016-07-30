fd720679fc8365f75105e69bad8e8d080f7d69e3
DataTalks <datatalks@DataTalkss-MacBook-Pro.local>
2016-07-31 00:03:19 +0800

    xiaofan

--- previewR.R
+++ previewR.R
@@ -3,7 +3,7 @@ Sys.setlocale(, "zh_CN.UTF-8")
 library(knitr)
 library(markdown)
 
-setwd("/Users/datatalks/DT/Dev/playR/MarkDown/RMD/$fileR")
+setwd("/home/playR/MarkDown/RMD/$fileR")
 
 
 knit("$fileR.Rmd")
