7d50a4e2705d59ad27e6e172283e76a33936478c
DataTalks <datatalks@DataTalkss-MacBook-Pro.local>
2016-08-05 17:20:41 +0800

    xiaofan

--- app/env/env_server.scala
+++
@@ -1,9 +0,0 @@
-package env
-
-
-object env_server {
-
-   val dir = "/home/playR"
-
-   val host = "123.57.162.246"
-}
--- app/env/env_xiaofan.scala
+++
@@ -1,9 +0,0 @@
-package env
-
-
-object env_xiaofan {
-
-   val dir = "/Users/datatalks/DT/Dev/playR"
-
-   val host = "localhost"
-}
