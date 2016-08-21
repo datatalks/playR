a4427ff14c75a78ec9fdab98339c9190a33aa4cd
DataTalks <datatalks@DataTalkss-MacBook-Pro.local>
2016-08-21 08:48:40 +0800

    xiaofan

--- app/controllers/WeixinController.scala
+++ app/controllers/WeixinController.scala
@@ -3,10 +3,11 @@ package controllers
 import play.Logger
 import play.api.mvc._
 
+import scala.concurrent.Future
 
 class WeixinController extends Controller {
 
-  def signature = Action { request =>
+  def xmlfeedback = Action { request =>
     request.body.asXml.map { xml =>
       println("the request from the weixin is :" + xml)
       (xml \\ "signature" headOption).map(_.text).map { signature =>
@@ -19,10 +20,10 @@ class WeixinController extends Controller {
     }
   }
 
-
-  def checkToken(signature: String, timestamp: String, nonce: String, echostr: String) = Action {
+  def checkToken(signature: String, timestamp: String, nonce: String, echostr: String) = Action.async { implicit request =>
     Logger.info("receive weixin server pamameter signature=" + signature + ",timestamp=" + timestamp + ",nonce=" + nonce + ",echostr=" + echostr)
-      Ok(echostr)
+    Future.successful(Ok(echostr))
+
   }
 
 }
