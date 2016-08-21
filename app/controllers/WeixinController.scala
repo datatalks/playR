83d565177aee0a1adb00b0310fb830e69d4dde08
DataTalks <datatalks@DataTalkss-MacBook-Pro.local>
2016-08-21 09:46:54 +0800

    xiaofan

--- app/controllers/WeixinController.scala
+++ app/controllers/WeixinController.scala
@@ -7,7 +7,13 @@ import scala.concurrent.Future
 
 class WeixinController extends Controller {
 
-  def xmlfeedback = Action { request =>
+  def checkToken(signature: String, timestamp: String, nonce: String, echostr: String) = Action.async { implicit request =>
+    Logger.info("receive weixin server pamameter signature=" + signature + ",timestamp=" + timestamp + ",nonce=" + nonce + ",echostr=" + echostr)
+    Future.successful(Ok(echostr))
+
+  }
+
+  def xmlrequest = Action { request =>
     request.body.asXml.map { xml =>
       println("the request from the weixin is :" + xml)
       (xml \\ "signature" headOption).map(_.text).map { signature =>
@@ -20,10 +26,24 @@ class WeixinController extends Controller {
     }
   }
 
-  def checkToken(signature: String, timestamp: String, nonce: String, echostr: String) = Action.async { implicit request =>
-    Logger.info("receive weixin server pamameter signature=" + signature + ",timestamp=" + timestamp + ",nonce=" + nonce + ",echostr=" + echostr)
-    Future.successful(Ok(echostr))
-
+  def xmlreponse = Action.async(parse.xml) { implicit request =>
+    println( "Forget the request header, the request body is: " + request.body)
+    val weixin = (request.body \\ "ToUserName" headOption).map(_.text).getOrElse("X")
+    val openid = (request.body \\ "FromUserName" headOption).map(_.text).getOrElse("X")
+    val CreateTime = (request.body \\ "CreateTime" headOption).map(_.text).getOrElse("X")
+    val MsgType = (request.body \\ "MsgType" headOption).map(_.text).getOrElse("X")
+    val Content = (request.body \\ "Content" headOption).map(_.text).getOrElse("X")
+    println(weixin)
+    println(openid)
+    Future.successful(Ok(
+      <xml>
+        <ToUserName>{openid}</ToUserName>
+        <FromUserName>{weixin}</FromUserName>
+        <CreateTime>{CreateTime}</CreateTime>
+        <MsgType>{MsgType}</MsgType>
+        <Content><![CDATA[你XXXXXX好]]></Content>
+      </xml>
+    ))
   }
 
 }
