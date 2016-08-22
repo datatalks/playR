bfe07b82c2808ad3845b93d149082c7770d67d67
DataTalks <datatalks@DataTalkss-MacBook-Pro.local>
2016-08-22 09:33:22 +0800

    xiaofan

--- app/env/env.scala
+++ app/env/env.scala
@@ -4,6 +4,6 @@ object env {
 
    val dir =   if(System.getenv("HOME") == "/root"){"/home/playR"} else { "/Users/datatalks/DT/Dev/playR" }
 
-   val host =  if(System.getenv("HOME") == "/root"){"123.57.162.246"} else { "localhost" }
+   val host =  if(System.getenv("HOME") == "/root"){"playr.data-talks.com"} else { "localhost" }
 
 }
